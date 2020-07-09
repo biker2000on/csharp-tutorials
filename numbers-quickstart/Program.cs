4ec52fb25fb4692af03d4f66569313559d5427e2
Justin Crawford <bikerhiker10@gmail.com>
2020-07-09 18:29:08 -0400

    inital commit of beginner projects

---
+++ numbers-quickstart/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 2;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+            Console.WriteLine(c.GetType());
+            int d = (int)Math.Pow((double)a,(double)b);
+            Console.WriteLine(d);
+            // c = a ** b;
+            // Console.WriteLine(c);
+        }
+    }
+}
