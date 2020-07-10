3908acce33899e13da2a6352d77c1ef7402d5644
Justin Crawford <bikerhiker10@gmail.com>
2020-07-09 20:07:21 -0400

    more numbers

--- numbers-quickstart/Program.cs
+++ numbers-quickstart/Program.cs
@@ -6,17 +6,27 @@ namespace NumbersInCSharp
   {
     static void Main(string[] args)
     {
-      int a = 7;
-      int b = 4;
-      int c = 3;
-      int d = (a + b) / c;
-      int e = (a + b) % c;
-      Console.WriteLine($"quotient: {d}");
-      Console.WriteLine($"remainder: {e}");
-      int max = int.MaxValue;
-      int min = int.MinValue;
-      Console.WriteLine($"The range of integers is {min} to {max}");
-      Console.WriteLine($"What happens if I add 1 to max: {max + 1}");
+      double a = 5;
+      double b = 4;
+      double c = 2;
+      double d = (a + b) / c;
+      Console.WriteLine(d);
+      double e = 19;
+      double f = 23;
+      double g = 8;
+      double h = (e + f) / g;
+      Console.WriteLine(h);
+      double max = double.MaxValue;
+      double min = double.MinValue;
+      Console.WriteLine($"The range of double is {min} to {max} and wraps {max + 0.01}");
+      double third = 1.0 / 3.0;
+      Console.WriteLine(third);
+      double one = 1.0;
+      Console.WriteLine(one);
+decimal minD = decimal.MinValue;
+decimal maxD = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {minD} to {maxD}");
+
     }
   }
 }
