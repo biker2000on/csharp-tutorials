24f132d5e5f872ff256d6f2c340af02f52ca0ebc
Justin Crawford <bikerhiker10@gmail.com>
2020-07-10 15:46:00 -0400

    branches tutorial

--- branches-tutorial/Program.cs
+++ branches-tutorial/Program.cs
@@ -2,11 +2,66 @@
 
 namespace BranchesAndLoops
 {
-    class Program
+  class Program
+  {
+    static void ExploreIf()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      int a = 5;
+      int b = 3;
+      if (a + b > 10)
+      {
+        Console.WriteLine("The answer is greater than 10");
+      }
+      else
+      {
+        Console.WriteLine("The answer is not greater than 10");
+      }
+
+      int c = 4;
+      if ((a + b + c > 10) && (a > b))
+      {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("And the first number is greater than the second");
+      }
+      else
+      {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("Or the first number is not greater than the second");
+      }
+
+      if ((a + b + c > 10) || (a > b))
+      {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("Or the first number is greater than the second");
+      }
+      else
+      {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("And the first number is not greater than the second");
+      }
     }
+
+    static void Main(string[] args)
+    {
+      // ExploreIf();
+      // int counter = 0;
+      // do
+      // {
+      //   Console.WriteLine($"The counter is {counter}");
+      //   counter++;
+      // } while (counter <= 10);
+
+      // for (int i = 0; i <= 10; i++)
+      // {
+      //   Console.WriteLine($"Hello World! Index is {i}");
+      // }
+
+      for (int j = 0; j <= 10; j++) {
+        for (int k = 0; k <= 10; k++) {
+          Console.Write($"({j},{k}), ");
+        }
+        Console.Write('\n');
+      }
+    }
+  }
 }
