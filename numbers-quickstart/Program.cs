5b5391df002efd37d927d49c17957907447c8a3e
Justin Crawford <bikerhiker10@gmail.com>
2020-07-09 20:12:08 -0400

    numbers finished with area

--- numbers-quickstart/Program.cs
+++ numbers-quickstart/Program.cs
@@ -4,29 +4,23 @@ namespace NumbersInCSharp
 {
   class Program
   {
+    static void CircleArea()
+    {
+      double r = 2.50;
+      double area = Math.PI * r * r;
+      Console.WriteLine(area);
+    }
     static void Main(string[] args)
     {
-      double a = 5;
-      double b = 4;
-      double c = 2;
-      double d = (a + b) / c;
-      Console.WriteLine(d);
-      double e = 19;
-      double f = 23;
-      double g = 8;
-      double h = (e + f) / g;
-      Console.WriteLine(h);
-      double max = double.MaxValue;
-      double min = double.MinValue;
-      Console.WriteLine($"The range of double is {min} to {max} and wraps {max + 0.01}");
-      double third = 1.0 / 3.0;
-      Console.WriteLine(third);
-      double one = 1.0;
-      Console.WriteLine(one);
-decimal minD = decimal.MinValue;
-decimal maxD = decimal.MaxValue;
-Console.WriteLine($"The range of the decimal type is {minD} to {maxD}");
+      double a = 1.0;
+      double b = 3.0;
+      Console.WriteLine(a / b);
 
+      decimal c = 1.0M;
+      decimal d = 3.0M;
+      Console.WriteLine(c / d);
+
+      CircleArea();
     }
   }
 }
